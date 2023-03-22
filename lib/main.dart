2d2ac0a0f5246feeb637b25befd8e04b7335a946
tannvv <nvtan.a5@gmail.com>
2023-03-23 00:46:19 +0700

    issue package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,6 +25,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseAuth auth = FirebaseAuth.instance;
+  await auth.setSettings(appVerificationDisabledForTesting: true);
 
   await dotenv.load(fileName: '.env');
 
